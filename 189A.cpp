7169cc7b6b1e68d109a5772036894db007a7da0e
amrutha <amrutha261001@gmail.com>
2021-01-30 22:31:18 +0530

    Jan 30

---
+++ 189A.cpp
@@ -0,0 +1,24 @@
+// Cut Ribbon
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int a,b,c,n;
+    cin>>n>>a>>b>>c;
+    int i,p=0,x,y,z;
+    i=min(a,min(b,c));
+    i=n/i;
+    for(x=i;x>=0;x--)
+    {
+        for(y=0;y<=i;y++)
+        {
+            z=abs((n-(a*x+b*y))/c);
+            if((a*x+b*y+c*z)==n)
+            {
+                p=max(p,x+y+z);
+            }
+        }
+    }
+    cout<<p<<endl;
+    return 0;
+}

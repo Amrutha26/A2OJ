0bc8034fee8cf2517f692496a0f9786bff92c62b
Amrutha <62595123+Amrutha26@users.noreply.github.com>
2021-02-22 22:44:43 +0530

    Feb 22

---
+++ 61A.cpp
@@ -0,0 +1,13 @@
+//Ultra-Fast Mathematician
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    string a,b;
+    cin >> a >> b;
+    for(int i=0; i < a.length(); i++) {
+        cout << (a[i] == b[i]? 0:1);
+    }
+    return 0;
+}

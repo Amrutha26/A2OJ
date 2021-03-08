7f732fcafd54ee3ce314b9f31bfc6ef02f7249ed
amrutha <amrutha261001@gmail.com>
2021-03-08 21:20:05 +0530

    March 8

---
+++ .vscode/617A.cpp
@@ -0,0 +1,19 @@
+//Elephant
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+   int n;
+   cin >> n;
+   if(n%5 == 0) {
+       cout << n/5;
+   } else {
+       cout << n/5 + 1;
+   }
+
+   return 0;
+}
+
+
+
